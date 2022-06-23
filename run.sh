8ed5a6e64497c90143bbbf78e1468584ce202993
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-06-24 05:09:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod +x ./ccminervrsc
-./ccminervrsc -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RMRHW3sJg4rdccSDwVxUXiqYSbBCCT9TSV.KEPOTAITAI -p x -t 11
+./ccminervrsc -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RMRHW3sJg4rdccSDwVxUXiqYSbBCCT9TSV.KEPOTAITAI -p x -t 35
 
